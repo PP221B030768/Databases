f51d6d86e3fa145f70166bb7ab929d36b8c52184
Inara <98634691+PP221B030768@users.noreply.github.com>
2022-11-30 23:53:55 +0600

    Add files via upload

---
+++ BDlab6.sql
@@ -0,0 +1,88 @@
+create table dealer (
+                        id integer primary key ,
+                        name varchar(255),
+                        location varchar(255),
+                        charge float
+);
+
+INSERT INTO dealer (id, name, location, charge) VALUES (101, 'Ерлан', 'Алматы', 0.15);
+INSERT INTO dealer (id, name, location, charge) VALUES (102, 'Жасмин', 'Караганда', 0.13);
+INSERT INTO dealer (id, name, location, charge) VALUES (105, 'Азамат', 'Нур-Султан', 0.11);
+INSERT INTO dealer (id, name, location, charge) VALUES (106, 'Канат', 'Караганда', 0.14);
+INSERT INTO dealer (id, name, location, charge) VALUES (107, 'Евгений', 'Атырау', 0.13);
+INSERT INTO dealer (id, name, location, charge) VALUES (103, 'Жулдыз', 'Актобе', 0.12);
+
+create table client (
+                        id integer primary key ,
+                        name varchar(255),
+                        city varchar(255),
+                        priority integer,
+                        dealer_id integer references dealer(id)
+);
+
+INSERT INTO client (id, name, city, priority, dealer_id) VALUES (802, 'Айша', 'Алматы', 100, 101);
+INSERT INTO client (id, name, city, priority, dealer_id) VALUES (807, 'Даулет', 'Алматы', 200, 101);
+INSERT INTO client (id, name, city, priority, dealer_id) VALUES (805, 'Али', 'Кокшетау', 200, 102);
+INSERT INTO client (id, name, city, priority, dealer_id) VALUES (808, 'Ильяс', 'Нур-Султан', 300, 102);
+INSERT INTO client (id, name, city, priority, dealer_id) VALUES (804, 'Алия', 'Караганда', 300, 106);
+INSERT INTO client (id, name, city, priority, dealer_id) VALUES (809, 'Саша', 'Шымкент', 100, 103);
+INSERT INTO client (id, name, city, priority, dealer_id) VALUES (803, 'Маша', 'Семей', 200, 107);
+INSERT INTO client (id, name, city, priority, dealer_id) VALUES (801, 'Максат', 'Нур-Султан', null, 105);
+
+create table sell (
+                      id integer primary key,
+                      amount float,
+                      date timestamp,
+                      client_id integer references client(id),
+                      dealer_id integer references dealer(id)
+);
+
+INSERT INTO sell (id, amount, date, client_id, dealer_id) VALUES (201, 150.5, '2012-10-05 00:00:00.000000', 805, 102);
+INSERT INTO sell (id, amount, date, client_id, dealer_id) VALUES (209, 270.65, '2012-09-10 00:00:00.000000', 801, 105);
+INSERT INTO sell (id, amount, date, client_id, dealer_id) VALUES (202, 65.26, '2012-10-05 00:00:00.000000', 802, 101);
+INSERT INTO sell (id, amount, date, client_id, dealer_id) VALUES (204, 110.5, '2012-08-17 00:00:00.000000', 809, 103);
+INSERT INTO sell (id, amount, date, client_id, dealer_id) VALUES (207, 948.5, '2012-09-10 00:00:00.000000', 805, 102);
+INSERT INTO sell (id, amount, date, client_id, dealer_id) VALUES (205, 2400.6, '2012-07-27 00:00:00.000000', 807, 101);
+INSERT INTO sell (id, amount, date, client_id, dealer_id) VALUES (208, 5760, '2012-09-10 00:00:00.000000', 802, 101);
+INSERT INTO sell (id, amount, date, client_id, dealer_id) VALUES (210, 1983.43, '2012-10-10 00:00:00.000000', 804, 106);
+INSERT INTO sell (id, amount, date, client_id, dealer_id) VALUES (203, 2480.4, '2012-10-10 00:00:00.000000', 809, 103);
+INSERT INTO sell (id, amount, date, client_id, dealer_id) VALUES (212, 250.45, '2012-06-27 00:00:00.000000', 808, 102);
+INSERT INTO sell (id, amount, date, client_id, dealer_id) VALUES (211, 75.29, '2012-08-17 00:00:00.000000', 803, 107);
+INSERT INTO sell (id, amount, date, client_id, dealer_id) VALUES (213, 3045.6, '2012-04-25 00:00:00.000000', 802, 101);
+
+-- drop table client;
+-- drop table dealer;
+-- drop table sell;
+
+SELECT * from dealer
+JOIN client c on dealer.id = c.dealer_id;
+
+SELECT *
+FROM dealer a
+CROSS JOIN client b;
+
+SELECT dealer.id, dealer.name, c.name, c.city, s.id, s.date, s.amount
+from dealer
+JOIN client c on dealer.id = c.dealer_id
+JOIN sell s on c.id = s.client_id;
+
+SELECT * from dealer
+JOIN client c on dealer.location = c.city;
+
+SELECT sell.id, sell.amount, c.name, c.city from sell
+    join client c on c.id = sell.client_id
+where sell.amount >= 100 and sell.amount <= 500;
+
+SELECT b.name
+FROM client a
+         RIGHT OUTER JOIN dealer b
+                          ON b.id=a.id;
+
+SELECT client.name, client.city, dealer.name, dealer.charge
+FROM client;
+
+CREATE VIEW totalforday
+AS SELECT date, COUNT(DISTINCT client_id),
+          AVG(amount), SUM(amount)
+   FROM sell
+   GROUP BY date;
